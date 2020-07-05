7922ca85e6e67488d64bb0dd34ae3ce41e0ef59e
Rafael <37466798+likeavenus@users.noreply.github.com>
2020-07-05 10:40:39 +0300

    master | in progress

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,10 +4,7 @@ export function fetchData() {
     return async dispatch => {
         const response = await fetch('https://api.opendota.com/api/matches/271145478');
         const data = await response.json();
+        // console.log(data.chat);
         dispatch({ type: GET_DATA, payload: data.chat })
-        // return {
-        //     type: GET_DATA,
-        //     payload: response
-        // }
     }
 }
